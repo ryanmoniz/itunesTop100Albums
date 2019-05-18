7c259ea5a3abdae029e9b5386784a34dbbc6dfa4
Ryan Moniz <ryanmoniz@gmail.com>
2019-05-18 11:19:06 -0700

    adding unit test to test JSON models for parsing

--- TopAlbums/TopAlbumsTests/TopAlbumsTests.swift
+++ TopAlbums/TopAlbumsTests/TopAlbumsTests.swift
@@ -10,25 +10,31 @@ import XCTest
 @testable import TopAlbums
 
 class TopAlbumsTests: XCTestCase {
-
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testJsonParser() {
+        
+        let bundle = Bundle(for: type(of: self))
+        guard let _jsonFile = bundle.url(forResource: "top100albums", withExtension: "json") else {
+            XCTFail("Missing file: top100albums.json")
+            return
+        }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        do {
+            let jsonData = try Data(contentsOf: _jsonFile)
+            let albums = try JSONDecoder().decode(Album.self, from: jsonData)
+            
+            
+            
+        } catch {
+            XCTFail("error loading contents of json file.\(error.localizedDescription)")
         }
     }
-
 }
