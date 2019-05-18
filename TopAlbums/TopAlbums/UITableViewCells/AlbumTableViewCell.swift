c90ba781cb1d6169b3c869e11ec08a4054e59653
Ryan Moniz <ryanmoniz@gmail.com>
2019-05-18 14:02:28 -0700

    images now loading for artwork

--- TopAlbums/TopAlbums/UITableViewCells/AlbumTableViewCell.swift
+++ TopAlbums/TopAlbums/UITableViewCells/AlbumTableViewCell.swift
@@ -16,9 +16,14 @@ class AlbumTableViewCell: UITableViewCell {
     @IBOutlet weak var artistName: UILabel!
     @IBOutlet weak var albumArtwork: UIImageView!
 
+    var session: URLSession!
+    var task: URLSessionDownloadTask!
+    var cache:NSCache<AnyObject, AnyObject> = NSCache()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        session = URLSession.shared
+        task = URLSessionDownloadTask()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -33,6 +38,25 @@ class AlbumTableViewCell: UITableViewCell {
         
         if let imageURL = viewModel.albumArtURL {
             //dispatch lazy load of image
+            
+            task = session.downloadTask(with: imageURL, completionHandler: { (location, response, error) in
+                if let _location = location {
+                    do {
+                        let data = try Data(contentsOf: _location)
+                        DispatchQueue.main.async {
+                            let img = UIImage(data: data)
+                            self.albumArtwork.image = img
+                        }
+                    } catch {
+                        NSLog("error:\(error.localizedDescription)")
+                        self.albumArtwork.image = UIImage(named: "album-unknown")
+                    }
+                } else {
+                    NSLog("could not download album artwork?")
+                    self.albumArtwork.image = UIImage(named: "album-unknown")
+                }
+            })
+            task.resume()
         } else {
             self.albumArtwork.image = UIImage(named: "album-unknown")
         }
